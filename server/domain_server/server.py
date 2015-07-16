5d8f1cbf79101fe6b6e87c0a52a621744988cdd8
belsberry <ben.elsberry@gmail.com>
2015-07-16 16:00:24 -0500

    Message Queue stub

--- server/domain_server/server.py
+++ server/domain_server/server.py
@@ -1,1 +1,24 @@
+import pika
+
 print("starting domain server")
+
+connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
+
+channel = connection.channel()
+
+channel.queue_declare(queue="hello")
+channel.queue_declare(queue="replicate")
+
+def callback(ch, method, properties, body):
+
+    print("[x] Received {0}".format(body))
+    channel.basic_publish(exchange="",
+                          routing_key="replicate",
+                          body="Replicated: {0}".format(body))
+
+
+channel.basic_consume(callback,
+                      queue="hello",
+                      no_ack=True)
+
+channel.start_consuming()
--- server/replicate_server/server.py
+++ server/replicate_server/server.py
@@ -1,1 +1,19 @@
+import pika
+
 print("starting replicate server")
+
+connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
+
+channel = connection.channel()
+
+channel.queue_declare(queue="replicate")
+
+
+def callback(ch, method, properties, body):
+    print("[x] Received {0}".format(body))
+
+channel.basic_consume(callback,
+                      queue="replicate",
+                      no_ack=True)
+
+channel.start_consuming()
